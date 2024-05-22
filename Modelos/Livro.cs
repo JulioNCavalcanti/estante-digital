ba9a3e1a4431f6be1d9537202f1fb57b7e366529
Júlio Nunes Cavalcanti <jnc.nunescavalcanti@outlook.com>
2024-05-22 09:40:36 -0300

    Adicionar modelo Livro

---
+++ Modelos/Livro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstanteDigital.Modelos;
+
+internal class Livro
+{
+    public Livro(string titulo, string autor, string descricao)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        Descricao = descricao;
+    }
+
+    private string Titulo { get; set; }
+    private string Autor { get; set; }
+    private string Descricao { get; set; }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Título: {Titulo}");
+        Console.WriteLine($" - Autor: {Autor}");
+        Console.WriteLine($" - Descrição: {Descricao}");
+        Console.WriteLine();
+    }
+
+    public static void ExibirTodosOsLivros(List<Livro> livros)
+    {
+        foreach (var livro in livros)
+        {
+            livro.ExibirDetalhes();
+        }
+    }
+}
